29fd13304db67e79644dbb1f7fe3204160f231dc
Tomaszewski-Mateusz <mateusztomm@gmail.com>
2023-03-27 21:59:32 +0200

    enqueue style fix

--- include/stylesheets.php
+++ include/stylesheets.php
@@ -1,9 +1,9 @@
 <?php
 // register webpack compiled css with theme
-function enqueue_webpack_stylesheets()
+function webpack_stylesheets()
 {
   $theme_version = wp_get_theme()->get('Version');
-  wp_enqueue_style('libs', get_template_directory_uri() . '/assets/css/libs.css', array(), $theme_version, true);
-  wp_enqueue_style('theme', get_template_directory_uri() . '/assets/css/app.css', array(), $theme_version, true);
+  wp_enqueue_style('libs', get_template_directory_uri() . '/assets/css/libs.css', array(), $theme_version);
+  wp_enqueue_style('theme', get_template_directory_uri() . '/assets/css/app.css', array(), $theme_version);
 }
-add_action('wp_enqueue_scripts', 'enqueue_webpack_stylesheets');
+add_action('wp_enqueue_scripts', 'webpack_stylesheets');

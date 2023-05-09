b50ca203fb42e0aaf3b0e4336cbc7613e0de233a
Mateusz Tomaszewski <matth.soft@gmail.com>
2023-05-09 14:56:52 +0200

    navwalker-fix

--- include/menus.php
+++ include/menus.php
@@ -1,4 +1,8 @@
 <?php
+function register_navwalker()
+{
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
 add_action('after_setup_theme', 'register_navwalker');
 
 register_nav_menus(
